0f06d2f31400f5c01abc7b87eb7d12c9f6d1247a
DurasovN <nikolay.durasov@gmail.com>
2024-05-23 21:50:40 +0400

    add elements_count

--- functional_programming.py
+++ functional_programming.py
@@ -6,14 +6,26 @@
 
 def array_sum(arr):
     """
-    :param arr: array of number
+    :param arr: array of numbers
     :return: numbers sum of array
     """
     if arr == []:
         return 0
     return arr[0] + array_sum(arr[1:])
 
-print(array_sum([1, 2, 3]))
+#print(array_sum([1, 2, 3]))
+
+
+def elements_count(arr):
+    """
+    :param arr: array of numbers
+    :return: numbers count of array
+    """
+    if arr == []:
+        return 0
+    return 1 + elements_count(arr[1:])
+
+#print(elements_count([1, 23, 3, 4, 3, 4, 1123, 2, 3]))
 
 
 
